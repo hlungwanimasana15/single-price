1ac42e3687deab800dab7d9644d032c5e2e0b5c3
YOUR NAME <YOUR@EMAIL.com>
2023-03-06 17:22:38 +0200

    single grid

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    
+let heading1='Join our community';
+let para1 ='30-day, hassle-free money back guarantee';
+let para2 =' Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.';
+let heading2='Monthly Subscription';
+let para12 ='&dollar;29 per month';
+let para13='Full access for less than &dollar;1 a day';
+let heading3='Why Us';
+let para33='industry experts Peer &amp; expert code review Coding exercises Access to our GitHub reposCommunity forumFlashcard decksNew videos every week';
+
+
+document.getElementById("heading1").innerHTML=heading1;
+document.getElementById("para1").innerHTML= para1;
+document.getElementById("para2").innerHTML=para2;
+document.getElementById("heading2").innerHTML=heading2;
+document.getElementById("para12").innerHTML= para12;
+document.getElementById("para13").innerHTML= para13;
+document.getElementById("heading3").innerHTML=heading3;
+document.getElementById("para33").innerHTML=para33;
+
+function myFunction() {
+    var x = document.getElementById("myBtn");
+    x.disabled = true;
+}
+}
